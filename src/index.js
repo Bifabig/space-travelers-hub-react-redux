9ca8b187e87648b82219a0ffa224fcca5ca4be60
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-19 17:15:55 -0300

    Delete Provider (Fix Temporarily)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import './index.css';
+// import { Provider } from 'react-redux';
 import App from './App';
 // import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <Provider>
-        <App />
-      </Provider>
+      <App />
     </Router>
   </React.StrictMode>,
 );
