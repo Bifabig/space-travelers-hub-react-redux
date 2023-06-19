455f3be86bed30b3247a15a409ef7da8505e53c9
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-19 16:57:12 -0300

    Add navbar (change extension to .jsx)

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from 'react-router-dom';
+import Icon from '../Assets/Icon.png';
+
+const links = [
+  { path: '/rockets', text: 'Rockets' },
+  { path: '/missons', text: 'Missons' },
+  { path: '/profile', text: 'Profile' },
+];
+const Navbar = () => (
+  <nav className="navbar-wrapper">
+    <div className="navbar">
+      <img src={Icon} alt="Icon" />
+      <h1>Space Travelers&apos; Hub</h1>
+      <ul>
+        {links.map((link) => (
+          <li key={link.text}>
+            <NavLink to={link.path}>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </nav>
+);
+export default Navbar;
