ed60d6a53db44aa5e85e2751ada03ea5cdcf4913
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-19 15:29:40 -0300

    Import Router, Provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 
-const App = () => {
-  return <div>App</div>;
-};
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      {/* <Route index element={<Rockets />} />
+      <Route path="rockets" element={<Rockets />} />
+      <Route path="missons" element={<Missons />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="*" element={<NotMatch />} /> */}
+    </Route>
+  </Routes>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+// import { store } from './redux/store.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Provider>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
 );
