ba209ae484e7dc02807e7375c963b5bed1b0781e
Bifabig <biftubig@gmail.com>
2023-06-20 19:08:36 +0300

    turn index css into module.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import './styles/index.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './components/redux/store';
