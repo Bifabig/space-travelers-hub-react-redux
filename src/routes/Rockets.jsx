c9cbf93317faa835f1f9e83b211ad00d0578a109
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-21 11:50:40 -0300

    Render elements and add module.css

--- src/routes/Rockets.jsx
+++ src/routes/Rockets.jsx
@@ -1,27 +1,62 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getRockets } from '../components/redux/rockets/rocketsSlice';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import { reserveRocket, getRockets } from '../components/redux/rockets/rocketsSlice';
 
 const Rockets = () => {
   const dispatch = useDispatch();
   const { rockets, isLoading, error } = useSelector((state) => state.rockets);
-  console.log(rockets);
   useEffect(() => {
     if (rockets.length === 0) {
       dispatch(getRockets());
     }
   }, [dispatch, rockets.length]);
 
+
   if (error) return <h2>Something went wrong</h2>;
 
   return isLoading ? (
     <h1>Loading...</h1>
   ) : (
-    <div className="rockets-form-wrapper">
+    <>
+      <hr />
       <div className="rockets-wrapper">
-        Rockets
+        {rockets.map((rocket) => (
+          <div key={rocket.rocket_id} className="rocket">
+            <img src={rocket.flickr_images[0]} alt="" />
+            <div className="rocket-info">
+              <h2>{rocket.rocket_name}</h2>
+              <p className="rocket-description">
+                {rocket.reserved ? (
+                  <span className="reserved">
+                    <Badge bg="success">Reserved</Badge>
+                  </span>
+                ) : null}
+
+                {rocket.description}
+              </p>
+              {rocket.reserved ? (
+                <Button
+                  variant="secondary"
+                >
+                  Cancel Reservation
+                </Button>
+              ) : (
+                <Button
+                  variant="primary"
+                  onClick={() => handleReserveRocket(rocket.rocket_id)}
+                >
+                  Reserve Rocket
+                </Button>
+              )}
+
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
+
+    </>
   );
 };
 export default Rockets;
