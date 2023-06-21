21752daf2c34429d2d85d424fb5b7a3c6a0de0c0
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-21 11:49:20 -0300

    Add dispatch action in rockets.jsx

--- src/routes/Rockets.jsx
+++ src/routes/Rockets.jsx
@@ -1,8 +1,27 @@
-const Rockets = () => (
-  <div className="rockets-form-wrapper">
-    <div className="rockets-wrapper">
-      Rockets
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRockets } from '../components/redux/rockets/rocketsSlice';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const { rockets, isLoading, error } = useSelector((state) => state.rockets);
+  console.log(rockets);
+  useEffect(() => {
+    if (rockets.length === 0) {
+      dispatch(getRockets());
+    }
+  }, [dispatch, rockets.length]);
+
+  if (error) return <h2>Something went wrong</h2>;
+
+  return isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className="rockets-form-wrapper">
+      <div className="rockets-wrapper">
+        Rockets
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default Rockets;
