61988e99b6e60754eb3ae6638c22acd353637ab0
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-22 15:52:36 -0300

    Implement render tests for Rockets

--- src/__tests__/Rockets.tests.jsx
+++ src/__tests__/Rockets.tests.jsx
@@ -25,4 +25,53 @@ describe('Rockets', () => {
       error: null,
     });
   });
+
+  it('should render loading message when isLoading is true', () => {
+    useSelector.mockReturnValue({
+      rockets: [],
+      isLoading: true,
+      error: null,
+    });
+
+    render(<Rockets />);
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('should render error message when error exists', () => {
+    useSelector.mockReturnValue({
+      rockets: [],
+      isLoading: false,
+      error: 'Something went wrong',
+    });
+
+    render(<Rockets />);
+
+    expect(screen.getByText('Something went wrong')).toBeInTheDocument();
+    const { container } = render(<Rockets />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render the rockets', () => {
+    useSelector.mockReturnValue({
+      rockets: [
+        {
+          rocket_id: '1',
+          rocket_name: 'Falcon 9',
+          flickr_images: ['image_url'],
+          reserved: false,
+          description: 'Rocket description',
+        },
+      ],
+      isLoading: false,
+      error: null,
+    });
+
+    const { container } = render(<Rockets />);
+
+    expect(screen.getByText('Falcon 9')).toBeInTheDocument();
+    expect(screen.getByText('Rocket description')).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
 });
