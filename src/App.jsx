7ebfc13f657424e0fb58e08aaeda865d96e72d60
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-22 04:47:59 -0300

    Make rockets the default page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Rockets from './routes/Rockets';
 import Missons from './routes/Missons';
@@ -8,6 +8,7 @@ import Profile from './routes/Profile';
 const App = () => (
   <Routes>
     <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Navigate to="/rockets" />} />
       <Route path="rockets" element={<Rockets />} />
       <Route path="missons" element={<Missons />} />
       <Route path="profile" element={<Profile />} />
