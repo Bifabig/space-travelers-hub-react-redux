f12bf7a33e7f8e8c4e5eece5a9f198af4e725d13
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-06-22 14:14:41 -0300

    Add data to axios mock

--- src/__tests__/__mocks__/axios.jsx
+++ src/__tests__/__mocks__/axios.jsx
@@ -1,3 +1,13 @@
 export default {
-  get: jest.fn().mockResolvedValue({ data: {} }),
+  get: jest.fn().mockResolvedValue({
+    data: [
+      {
+        id: 1,
+        rocket_name: 'Falcon 9',
+        description: 'A two-stage rocket designed and manufactured by SpaceX.',
+        image: 'falcon9.jpg',
+        reserved: false,
+      },
+    ],
+  }),
 };
