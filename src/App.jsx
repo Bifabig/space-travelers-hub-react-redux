fcb377dc121ead0ad64b26ea8c85cf7051c87352
Bifabig <biftubig@gmail.com>
2023-06-23 11:04:23 +0300

    fix file name typo

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Rockets from './routes/Rockets';
-import Missons from './routes/Missons';
 import Profile from './routes/Profile';
+import Missions from './routes/Missions';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Layout />}>
       <Route path="/" element={<Navigate to="/rockets" />} />
       <Route path="rockets" element={<Rockets />} />
-      <Route path="missons" element={<Missons />} />
+      <Route path="missions" element={<Missions />} />
       <Route path="profile" element={<Profile />} />
     </Route>
   </Routes>
